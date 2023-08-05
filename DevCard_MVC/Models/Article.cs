00a71ddc8674627bdd0debb677dc2428aa7d60d4
MohammadMahdi <MohammadMahdi@DESKTOP-8TT3VIO>
2023-08-05 14:36:29 +0330

    Projects From Index page Moved to PatialView Projects

---
+++ DevCard_MVC/Models/Article.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DevCard_MVC.Models
+{
+	public class Article
+	{
+        public long ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+   
+}
